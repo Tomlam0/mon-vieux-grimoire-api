ba78be954e4d0c9045773d2ffb0b9d95960c1054
Droubi1 <105873915+Droubi1@users.noreply.github.com>
2024-08-22 12:09:02 +0200

    chore: added min and max validation for email and password in signup

--- src/routes/user/logout.route.ts
+++ src/routes/user/logout.route.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 
 import { LogoutResponseSchema } from '@/schema/user/logout.schema';
 import { logout } from '@/controllers/user/index';
+import { ERROR401 } from '@/constants/response.constants';
 
 export async function logoutRoute(app: FastifyInstance) {
   // JWT is set on a cookie so we need to delete the cookie from backend on user logout
@@ -29,6 +30,7 @@ export async function logoutRoute(app: FastifyInstance) {
             },
           },
         },
+        401: ERROR401('Access denied. Please log in.'),
       },
     },
 
--- src/schema/user/signup.schema.ts
+++ src/schema/user/signup.schema.ts
@@ -6,16 +6,28 @@ import { z, TypeOf } from 'zod';
  * ========================================
  */
 export const SignupSchema = z.object({
-  // Email
-  email: z.string().email({
-    message: 'The email address provided is not valid. Please check and try again.',
-  }),
+  email: z
+    .string()
+    .email({
+      message: 'The email address provided is not valid. Please check and try again.',
+    })
+    .min(8, {
+      message: 'The email address must be at least 8 characters long.',
+    })
+    .max(128, {
+      message: 'The email address must be no longer than 128 characters.',
+    }),
 
-  // Password
   password: z
     .string()
     .min(8, {
       message: 'The password must be at least 8 characters long.',
+    })
+    .max(64, {
+      message: 'The password must be no longer than 64 characters.',
+    })
+    .refine((value) => !/\s/.test(value), {
+      message: 'The password must not contain spaces.',
     })
     .refine((value) => /[a-z]/.test(value), {
       message: 'The password must contain at least one lowercase letter.',
@@ -28,9 +40,6 @@ export const SignupSchema = z.object({
     })
     .refine((value) => /[@$!%*?&#]/.test(value), {
       message: 'The password must contain at least one special character.',
-    })
-    .refine((value) => !/\s/.test(value), {
-      message: 'The password must not contain spaces.',
     }),
 });
 
